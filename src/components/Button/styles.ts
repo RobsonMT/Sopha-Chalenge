c1354485a9ae3a94c30c20469bf0708f1a48b9fd
Robson Fernando Martins <rbsndev3@gmail.com>
2024-03-08 15:46:58 -0300

    feat: SignIn

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,12 +4,14 @@ export const Container = styled.div`
   button {
     background-color: var(--blue);
     color: var(--white);
-    font-weight: 700;
+    border: none;
+    border-radius: 0.5rem;
     font-size: 1.6rem;
-    height: 5rem;
+    height: 4rem;
     padding: 1rem;
     margin-top: 1rem;
     margin-bottom: 1rem;
+    cursor: pointer;
 
     &:hover {
       background-color: var(--blue);
@@ -18,6 +20,6 @@ export const Container = styled.div`
   }
 
   button > span {
-    font-size: 1.4rem;
+    font-size: 1.6rem;
   }
 `;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,66 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import Input from "../../components/Input";
+import { FaEnvelope, FaLock } from "react-icons/fa";
+import { Container, Wrapper } from "./styles";
+import { Button } from "../../components/Button";
+import { useNavigate } from "react-router-dom";
+
+export interface ISignIn {
+  email: string;
+  password: string;
+}
+
 const SignIn = () => {
-  return <div>Signin</div>;
+  const navigate = useNavigate();
+
+  const schema = yup.object().shape({
+    email: yup.string().email("Email inválido").required("Campo obrigatório"),
+    password: yup.string().required("Campo obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<ISignIn>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitFunc = (data: ISignIn) => {
+    console.log(data);
+    reset();
+    navigate("/dashboard");
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit(onSubmitFunc)}>
+          <Input
+            icon={FaEnvelope}
+            type="email"
+            placeholder="Email"
+            register={register}
+            name="email"
+            error={errors.email?.message}
+          />
+          <Input
+            icon={FaLock}
+            type="text"
+            placeholder="Senha"
+            register={register}
+            name="password"
+            error={errors.password?.message}
+          />
+          <Button type="submit">Enviar</Button>
+        </form>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default SignIn;
